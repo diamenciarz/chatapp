da5a4792b4504c5538f79c3793bcbcf7df937c0a
diamenciarz <stasostaszewski@gmail.com>
2025-03-04 20:03:52 +0100

    changed title

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Le Kahdo | (Alpha test)"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
